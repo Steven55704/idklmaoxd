92a47cec3f6cad5ba82f103d47a9267283cdf5ae
Steven55704 <94083149+Steven55704@users.noreply.github.com>
2022-03-08 10:28:39 +0800

    Create aimlock.lua

---
+++ Rush Point/aimlock.lua
@@ -0,0 +1,5 @@
+-- // Dependencies
+local Aiming = loadstring(game:HttpGet("https://raw.githubusercontent.com/Steven55704/idklmaoxd/main/Rush%20Point/GamePatches/load.lua"))()("Module", "RushPoint")
+
+-- // Start
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Steven55704/idklmaoxd/main/Rush%20Point/Example/aimlock.lua"))()
