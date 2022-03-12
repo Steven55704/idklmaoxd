c1edf5030b4458aa4cdc80d7c858c85f5c9e8129
Steven55704 <94083149+Steven55704@users.noreply.github.com>
2022-03-12 14:35:37 +0800

    Update karen.lua

--- karen.lua
+++ karen.lua
@@ -127,13 +127,13 @@ function Type(Word)
         for _, Letter in next, string.split(Word, "") do
             if math.random(1, 5) == 1 then
                 Typebox.Text ..= string.char(math.random(string.byte('A'), string.byte('Z')))
-                wait(WaitTime / 1.5)
+                wait(WaitTime / 2.0)
                 Typebox.Text ..= string.char(math.random(string.byte('A'), string.byte('Z')))
                 wait(WaitTime * 2.8)
                 Typebox.Text = Typebox.Text:sub(0, -2)
-                wait(WaitTime / 1.3)
+                wait(WaitTime / 1.8)
                 Typebox.Text = Typebox.Text:sub(0, -2)
-                wait(WaitTime / 1.3)
+                wait(WaitTime / 1.8)
                 Typebox.Text ..= Letter
                 wait(WaitTime)
             else
