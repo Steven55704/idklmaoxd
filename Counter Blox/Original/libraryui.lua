a18a0a9fc322738782139b24bce06949d2b210cc
Steven55704 <94083149+Steven55704@users.noreply.github.com>
2022-07-05 13:07:28 +0800

    Delete Counter Blox directory

--- Counter Blox/Original/libraryui.lua
+++
@@ -1,2 +0,0 @@
-getgenv().text = "Bloxtap"
-loadstring(game:HttpGet"https://raw.githubusercontent.com/dawid-scripts/UI-Libs/main/gamesensewatermark.lua")()
--- Counter Blox/ui.lua
+++
@@ -1,2 +0,0 @@
-getgenv().text = "Cbloxgui"
-loadstring(game:HttpGet"https://raw.githubusercontent.com/Steven55704/idklmaoxd/main/Counter%20Blox/libraryui.lua")()
